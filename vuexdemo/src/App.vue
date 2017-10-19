1e322bb6292b2b922430c4d4c10c1c2183d486cd
One Day丶 <614345508@qq.com>
2017-10-20 01:59:58 +0800

    集成了bootstrap and aswcomw

--- vuexdemo/src/App.vue
+++ vuexdemo/src/App.vue
@@ -1,16 +1,34 @@
 <template>
 	<div id="app">
-		<!-- Count is {{$store.state.count}} -->
-		<!-- Count is {{count}}
-		<button @click="increment">+5</button>
-		<button @click="decrement">-3</button> -->
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">Brand</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="#"><i class="fa fa-user-circle-o" aria-hidden="true"></i>个人中心</a>
+						</li>
+						<li>
+							<a href="#">退出</a>
+						</li>
+					</ul>
+				</div>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container-fluid -->
+		</nav>
 		<router-link to="/hello">hello</router-link>
-		 <router-view></router-view>
+		<router-view></router-view>
 	</div>
 </template>
 
 <script>
-	import {mapGetters,mapActions} from "vuex";
+	import { mapGetters, mapActions } from "vuex";
 	export default {
 		name: 'app',
 		data() {
@@ -18,13 +36,13 @@
 				msg: 'Welcome to Your Vue.js App'
 			}
 		},
-		computed:{
+		computed: {
 			...mapGetters([]),
-			count(){
+			count() {
 				return this.$store.state.count;
 			}
 		},
-		methods:{
+		methods: {
 			...mapActions([
 				'increment',
 				'decrement'
@@ -34,8 +52,5 @@
 </script>
 
 <style lang="less">
-
-   #app{
-
-   }
+	#app {}
 </style>
